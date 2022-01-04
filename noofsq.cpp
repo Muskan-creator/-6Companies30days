0da9f05ca2788090a8802d4b16877d59c8428262
Muskan <59757988+Muskan-creator@users.noreply.github.com>
2022-01-04 21:35:26 +0530

    Create noofsq.cpp

---
+++ noofsq.cpp
@@ -0,0 +1,33 @@
+// { Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution {
+  public:
+    long long squaresInChessBoard(long long N) {
+        long res =0;
+       while(N>=1){
+           res = res + (N*N);
+           N--;
+       }
+      return res; 
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.squaresInChessBoard(N) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
